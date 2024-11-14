b55d9d5f766861a5a9104f2efecd22573e158797
XinyangGu <1402759843@qq.com>
2024-11-14 18:11:52 +0800

    add levy_flight generation

--- Competition/DERAND.hpp
+++ Competition/DERAND.hpp
@@ -6,6 +6,12 @@
  *
  * @brief namespace derand 提供了随机数生成相关的函数
  *
+ * @brief 设置随机数种子 set_seed
+ * @brief 轮盘赌选择 roulette_wheel_selection
+ * @brief 随机交换两个元素 swap_two_element_randomly
+ * @brief levy flight步长生成 levy_flight
+ *
+ *
  * @example 使用示例
  * int main()
  * {
@@ -79,6 +85,29 @@ inline void swap_two_element_randomly(std::vector<T>& v)
     int i = engine() % v.size();
     int j = engine() % v.size();
     std::swap(v[i], v[j]);
+}
+
+/**
+ * @brief levy flight
+ * @attention 随机步长的运动模式，特点是间隔较短或偶尔较长的跳跃
+ *            levy分布，是一种重尾分布，是一种概率分布，其概率密度函数在正无穷大处有一个尾部
+ *            概率密度函数p(x) = 1 / (len ** (1 + beta)) 其中len是步长，beta是分布的形状参数
+ *            具体使用时，新的位置 = 旧的位置 + alpha * Levy(beta)
+ * @param beta 分布的形状参数
+ * @param scale 步长更新系数
+ */
+double levy_flight(double beta, double alpha)
+{
+    // 生成标准正态分布的两个独立变量 u 和 v
+    std::normal_distribution<double> normal(0.0, 1.0);
+    double u = normal(engine);
+    double v = normal(engine);
+
+    // 使用Mantegna的方法生成Levy分布
+    double sigma_u = pow((tgamma(1 + beta) * sin(M_PI * beta / 2)) / (tgamma((1 + beta) / 2) * beta * pow(2, (beta - 1) / 2)), 1.0 / beta);
+    double levy_step = alpha * (u * sigma_u) / pow(fabs(v), 1.0 / beta);
+
+    return levy_step;
 }
 } // namespace derand;
 
